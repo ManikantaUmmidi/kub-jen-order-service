8f83a59815ab1cff49f32744e86a92c38ee8a5fd
Ummidi Manikanta <umanikanta@nisum.com>
2026-08-12 12:17:16 +0530

    changes done

--- src/main/java/com/app/kub_jen_order_service/OrderController.java
+++ src/main/java/com/app/kub_jen_order_service/OrderController.java
@@ -19,7 +19,7 @@ public class OrderController {
     @GetMapping("/health")
     public String health() {
 
-        return "Order Service is running successfully";
+        return "Order Service is running";
     }
 
     @DeleteMapping("/{id}")
